ebc6a3f5bda75def5350364236067d2777916731
Kabana <160867547+FernandoFCarvalho@users.noreply.github.com>
2024-06-09 08:10:55 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,122 @@
+//1
+//function main(){
+    let btn1_1 = document.getElementById('btn1_1');
+    btn1_1.addEventListener('click', vdd1_1)
+    let btn1_2 = document.getElementById('btn1_2');
+    btn1_2.addEventListener('click', vdd1_2)
+        
+//}
+
+
+function vdd1_1(){
+    window.open("https://youtube.com/playlist?list=PLHz_AreHm4dmSj0MHol_aoNYCSGFqvfXV&si=iy62aFR4Apj_wuQh")
+}
+function vdd1_2(){
+    window.open("https://youtube.com/playlist?list=PLP6Z8YhN_d5QtwbnsTG9MkSwaikpCy9ks&si=jLHHWn8qq6H2l2WN")
+}
+
+
+//2
+let btn2_1 = document.getElementById('btn2_1');
+btn2_1.addEventListener('click', vdd2_1)
+let btn2_2 = document.getElementById('btn2_2');
+btn2_2.addEventListener('click', vdd2_2)
+let btn2_3 = document.getElementById('btn2_3');
+btn2_3.addEventListener('click', vdd2_3)
+
+function vdd2_1(){
+    window.open("https://www.youtube.com/watch?v=eaJwZLHURjg&list=PLvat2X-KHJNYmC4M1HHrDkMkFDf8Dwyf8&pp=iAQB")
+}
+function vdd2_2(){
+    window.open("https://youtube.com/playlist?list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6&si=28w4CazVRQYDa90p")
+}
+function vdd2_3(){
+    window.open("https://youtube.com/playlist?list=PLHz_AreHm4dkI2ZdjTwZA4mPMxWTfNSpR&si=kawoWpedrPhGMrxc")
+}
+
+
+
+//3
+let btn3_1 = document.getElementById('btn3_1');
+btn3_1.addEventListener('click', vdd3_1)
+let btn3_2 = document.getElementById('btn3_2');
+btn3_2.addEventListener('click', vdd3_2)
+
+function vdd3_1(){
+    window.open("https://youtube.com/playlist?list=PLE1UtdMhwaEonmSRDkSzpFV5m5zKiqM94&si=8o5lEKUc7bH8pBC7")
+}
+function vdd3_2(){
+    window.open("https://youtube.com/playlist?list=PLGxZ4Rq3BOBrgumpzz-l8kFMw2DLERdxi&si=b1_r6uCO1Rlz0wgJ")
+}
+
+
+
+//4
+let btn4_1 = document.getElementById('btn4_1');
+btn4_1.addEventListener('click', vdd4_1)
+let btn4_2 = document.getElementById('btn4_2');
+btn4_2.addEventListener('click', vdd4_2)
+
+function vdd4_1(){
+    window.open("https://youtube.com/playlist?list=PLHz_AreHm4dkZ9-atkcmcBaMZdmLHft8n&si=ehO2-_7h3OCtTWCs")
+}
+function vdd4_2(){
+    window.open("https://youtube.com/playlist?list=PLNb8WhOkH6MV9BZdkBz0FRNsC1CfTYgmX&si=VYYdLSNdv928RIE8")
+}
+
+
+
+//5
+let btn5_1 = document.getElementById('btn5_1');
+btn5_1.addEventListener('click', vdd5_1)
+let btn5_2 = document.getElementById('btn5_2');
+btn5_2.addEventListener('click', vdd5_2)
+
+function vdd5_1(){
+    window.open("https://youtu.be/wXMlMsDvk2M?si=KcD9kXdIaDmU34Ww")
+}
+function vdd5_2(){
+    window.open("https://youtu.be/Qjk-cSW-jk4?si=iYseMx-aHkOUQUWC")
+}
+
+
+//6
+let btn6_1 = document.getElementById('btn6_1');
+btn6_1.addEventListener('click', vdd6_1)
+let btn6_2 = document.getElementById('btn6_2');
+btn6_2.addEventListener('click', vdd6_2)
+
+function vdd6_1(){
+    window.open("https://www.youtube.com/watch?v=Ofktsne-utM&list=PLHz_AreHm4dkBs-795Dsgvau_ekxg8g1r")
+}
+function vdd6_2(){
+    window.open("https://www.youtube.com/playlist?list=PLucm8g_ezqNoNHU8tjVeHmRGBFnjDIlxD")
+}
+
+
+//7
+let btn7_1 = document.getElementById('btn7_1');
+btn7_1.addEventListener('click', vdd7_1)
+let btn7_2 = document.getElementById('btn7_2');
+btn7_2.addEventListener('click', vdd7_2)
+
+function vdd7_1(){
+    window.open("https://www.youtube.com/playlist?list=PLHz_AreHm4dm7ZULPAmadvNhH6vk9oNZA")
+}
+function vdd7_2(){
+    window.open("https://www.youtube.com/playlist?list=PLcoYAcR89n-qbO7YAVj5S0alABLis_QVU")
+}
+
+
+//8
+let btn8_1 = document.getElementById('btn8_1');
+btn8_1.addEventListener('click', vdd8_1)
+let btn8_2 = document.getElementById('btn8_2');
+btn8_2.addEventListener('click', vdd8_2)
+
+function vdd8_1(){
+    window.open("https://www.youtube.com/watch?v=9C3b3c4hYlY&list=PLuElAIt7y8x0MHwvS_ZqNDSpco6J1ZJvs")
+}
+function vdd8_2(){
+    window.open("https://www.youtube.com/playlist?list=PLHz_AreHm4dm6wYOIW20Nyg12TAjmMGT-")
+}
